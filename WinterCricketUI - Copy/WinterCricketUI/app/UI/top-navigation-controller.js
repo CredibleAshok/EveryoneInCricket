7ce465d9e3cdd3350f8eb6709403f4d5c21d4704
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-05-31 17:38:51 +0800

    Add files via upload

---
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/top-navigation-controller.js
@@ -0,0 +1,7 @@
+﻿//This controller controls the dynamic display of menu items or top level navegation.
+(function () {
+    myApp.controller('topNavigationCtrl', function ($scope, routes, $filter) {
+        var vm = this;
+        vm.routes = routes;
+    })
+})();
