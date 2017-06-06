5d7af5da9b8737f7de1a81ed1d284b5f16edf691
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-06 17:49:33 +0800

    Add files via upload

--- WinterCricketUI - Copy/WinterCricketUI/app/myApp.js
+++ WinterCricketUI - Copy/WinterCricketUI/app/myApp.js
@@ -1,1 +1,13 @@
 ﻿var myApp = angular.module('myApp', ['ui.router', 'smart-table', 'ngSanitize', 'ui.select']);
+
+myApp.run(['$state', 'securitySservice', '$q', function ($state, securitySservice, $q) {
+
+    // Get the current user when the application starts
+    // (in case they are still logged in from a previous session)
+    // Note! angular does not wait till this request completes, so it is repeated in the authorization code.
+    var defer = $q.defer();
+    securitySservice.getloggedInUser(defer);
+
+    return defer.promise;
+
+}])
