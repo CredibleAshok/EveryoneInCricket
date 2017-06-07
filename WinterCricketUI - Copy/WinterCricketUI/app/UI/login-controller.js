e0881602fbd89ca2311434745f64b0f30c31c8ec
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-07 17:26:34 +0800

    Add files via upload

---
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/login-controller.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    myApp.controller('loginCtrl', function ($scope, $http, config, $stateParams, securitySservice) {
+        var vm = this;
+        console.log("login controller loaded.");
+        vm.getUserDetails = function (userName, password) {
+            securitySservice.getloggedInUser(userName, password).then(function (resp) {
+                var currentUser1 = securitySservice.getcurrentUser();
+                console.log("securitySservice.currentUser:- " + securitySservice.getcurrentUser().Name);
+            });
+        };
+    });
+})();
--- WinterCricketUI - Copy/WinterCricketUI/app/UI/top-navigation-controller.js
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/top-navigation-controller.js
@@ -1,7 +1,8 @@
 ﻿//This controller controls the dynamic display of menu items or top level navegation.
 (function () {
-    myApp.controller('topNavigationCtrl', function ($scope, routes, $filter) {
+    myApp.controller('topNavigationCtrl', function ($scope, routes, $filter, securitySservice) {
         var vm = this;
         vm.routes = routes;
+        vm.currentUser = securitySservice.getcurrentUser();
     })
 })();
