5619fdf6ac88e4c5ed885a038917196e152bc06e
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-07 21:51:46 +0800

    Add files via upload

--- WinterCricketUI - Copy/WinterCricketUI/test-data/userData.js
+++ WinterCricketUI - Copy/WinterCricketUI/test-data/userData.js
@@ -17,5 +17,21 @@
             "FeatureId": 10,
             "Name": "View Team"
         }]
+    }, {
+        "RoleId": 2,
+        "Name": "Captain",
+        "Features": [{
+            "FeatureId": 1,
+            "Name": "Add Match"
+        }, {
+            "FeatureId": 4,
+            "Name": "View Result"
+        }, {
+            "FeatureId": 7,
+            "Name": "View Venue"
+        }, {
+            "FeatureId": 10,
+            "Name": "View Team"
+        }]
     }]
 }]
