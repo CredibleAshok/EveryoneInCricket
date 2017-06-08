b4a66a50b790f187f1c533e410a64a0c3f113d30
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-08 21:18:10 +0800

    Add files via upload

--- WinterCricketUI - Copy/WinterCricketUI/app/directives/dropdown-directives.js
+++ WinterCricketUI - Copy/WinterCricketUI/app/directives/dropdown-directives.js
@@ -70,5 +70,75 @@
         }
 
     }]);
+
+    myApp.directive('matchTypeDropdown', ['matchTypeSservice', '$timeout', function matchTypeDropdownDirectiveFunction(matchTypeSservice, $timeout) {
+        //Usage:
+        //<match-type-dropdown></match-type-dropdown>
+        var directive = {
+            link: link,
+            restrict: 'AE',
+            require: '^ngModel',
+            transclude: true,
+            templateUrl: 'app/directives/templates/match-type-tpl.html',
+            scope: {
+                'model': '=ngModel'
+            },
+        };
+        return directive;
+        function link(scope, element, attrs, ctrl) {
+            $timeout(function () {
+                matchTypeSservice.getMatchTypeList().then(function (resp) {
+                    scope.matcheTypesList = resp;
+                });
+            });
+
+            scope.$watch('model', function () {
+                scope.$eval(attrs.ngModel + ' = model');
+            });
+            scope.$watch(attrs.ngModel, function (val) {
+                scope.model = val;
+                ctrl.$setViewValue(val);
+                if (angular.isDefined(attrs.callFunc)) {
+                    scope.callFunc();
+                }
+            });
+        }
+
+    }]);
+
+    myApp.directive('venueDropdown', ['venuesSservice', '$timeout', function venueDropdownDirectiveFunction(venuesSservice, $timeout) {
+        //Usage:
+        //<venue-dropdown></venue-dropdown>
+        var directive = {
+            link: link,
+            restrict: 'AE',
+            require: '^ngModel',
+            transclude: true,
+            templateUrl: 'app/directives/templates/venue-dropdown-tpl.html',
+            scope: {
+                'model': '=ngModel'
+            },
+        };
+        return directive;
+        function link(scope, element, attrs, ctrl) {
+            $timeout(function () {
+                venuesSservice.getVenuesList().then(function (resp) {
+                    scope.venuesList = resp;
+                });
+            });
+
+            scope.$watch('model', function () {
+                scope.$eval(attrs.ngModel + ' = model');
+            });
+            scope.$watch(attrs.ngModel, function (val) {
+                scope.model = val;
+                ctrl.$setViewValue(val);
+                if (angular.isDefined(attrs.callFunc)) {
+                    scope.callFunc();
+                }
+            });
+        }
+
+    }]);
 })();
 
