8fe0231d382c1e6c15ae39204aac174a2ce468a2
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-08 17:23:50 +0800

    Add files via upload

---
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/add-match-controller.js
@@ -0,0 +1,11 @@
+﻿//addMatchCtrl
+(function () {
+    myApp.controller('addMatchCtrl', function ($scope, $http, $stateParams, playerSservice) {
+        var vm = this;
+       
+        var getProfile = function () {
+            console.log("addMatchCtrl controller loaded." );
+        };
+        getProfile();
+    });
+})();
