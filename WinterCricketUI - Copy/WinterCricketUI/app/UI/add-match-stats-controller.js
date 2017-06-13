855c3df45143d5f9eb66c7a7728f06fbc33cd0cc
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-13 17:26:18 +0800

    Add files via upload

---
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/add-match-stats-controller.js
@@ -0,0 +1,19 @@
+﻿//addMatchStatsCtrl
+(function () {
+    myApp.controller('addMatchStatsCtrl', function ($scope, $http, $stateParams, matchSservice) {
+        var vm = this;
+        vm.addMatchStats = {};
+        vm.addMatchStats.matchId = $stateParams.matchId;
+        vm.newResult = {};
+
+        var getTeamsByMatchId = function () {
+            vm.teamlist = [];
+            matchSservice.getTeamsByMatchId(vm.matchId).then(function (resp) {
+                vm.teamlist.push(resp[0].awayTeamDetails);
+                vm.teamlist.push(resp[0].homeTeamDetails);
+            });
+            console.log("addResultCtrl controller loaded.");
+        };
+        getTeamsByMatchId();
+    });
+})();
