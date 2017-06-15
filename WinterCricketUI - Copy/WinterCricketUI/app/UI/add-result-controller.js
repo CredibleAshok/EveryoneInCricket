faec35e886260dff9c705b4ad563c878d4f3758f
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-15 17:34:28 +0800

    Update add-result-controller.js

--- WinterCricketUI - Copy/WinterCricketUI/app/UI/add-result-controller.js
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/add-result-controller.js
@@ -1,10 +1,10 @@
-﻿//addResultCtrl
+//addResultCtrl
 (function () {
-    myApp.controller('addResultCtrl', function ($scope, $http, $stateParams, matchSservice) {
+    myApp.controller('addResultCtrl', function ($scope, $http, $stateParams, matchSservice, filterFilter) {
         var vm = this;
         vm.matchId = $stateParams.matchId;
         vm.newResult = {};
-
+        vm.winningModeList = [{ "id": 1, "name": "Wickets" }, { "id": 2, "name": "Runs" }];
         vm.saveResult = function () {
             resultSservice.saveNewVenue(vm.newResult).then(function (resp) {
                 console.log("new Venue saving passed.");
@@ -19,8 +19,18 @@
                 vm.teamlist.push(resp[0].awayTeamDetails);
                 vm.teamlist.push(resp[0].homeTeamDetails);
             });
-            //console.log("addResultCtrl controller loaded.");
         };
+
+        $scope.$watch('vm.newResult.wonBy', function (newVal, oldVal) {
+            if (newVal != undefined) {
+                angular.forEach(vm.teamlist, function (val, key) {
+                    if (val.teamId != newVal.teamId) {
+                        vm.newResult.lostBy = val;
+                    }
+                });
+            }
+        });
+
         getTeamsByMatchId();
     });
 })();
