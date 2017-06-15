6c5c83b76cc7ed6acb135b685075e22d9bbf63cb
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-15 17:37:10 +0800

    Update add-match-stats-controller.js

--- WinterCricketUI - Copy/WinterCricketUI/app/UI/add-match-stats-controller.js
+++ WinterCricketUI - Copy/WinterCricketUI/app/UI/add-match-stats-controller.js
@@ -1,11 +1,11 @@
-﻿//addMatchStatsCtrl
+//addMatchStatsCtrl
 (function () {
     myApp.controller('addMatchStatsCtrl', function ($scope, $http, $stateParams, matchSservice) {
         var vm = this;
         vm.addMatchStats = {};
         vm.addMatchStats.matchId = $stateParams.matchId;
         vm.newResult = {};
-
+        vm.plc = "My Search...";
         var getTeamsByMatchId = function () {
             vm.teamlist = [];
             matchSservice.getTeamsByMatchId(vm.addMatchStats.matchId).then(function (resp) {
@@ -14,6 +14,16 @@
             });
             console.log("addResultCtrl controller loaded.");
         };
+
+        $scope.$watch('vm.newMatchStats.homeTeam', function (newVal, oldVal) {
+            if (newVal != undefined) {
+                angular.forEach(vm.teamlist, function (val, key) {
+                    if (val.teamId != newVal.teamId) {
+                        vm.newMatchStats.awayTeam = val;
+                    }
+                });
+            }
+        });
         getTeamsByMatchId();
     });
 })();
