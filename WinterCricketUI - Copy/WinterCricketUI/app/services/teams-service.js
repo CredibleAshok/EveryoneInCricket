b41c587cd7bc8c3ce477a2492fb21648f342c02e
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2017-06-16 19:33:53 +0800

    Update teams-service.js

--- WinterCricketUI - Copy/WinterCricketUI/app/services/teams-service.js
+++ WinterCricketUI - Copy/WinterCricketUI/app/services/teams-service.js
@@ -1,4 +1,4 @@
-﻿// Name: teamsSservice
+// Name: teamsSservice
 // Type: Angular Service
 // Purpose: Provide list of players available, this is for single player.
 // Design: On initialisation this service loads the reference data from the server
@@ -12,11 +12,32 @@
 
         var service = {
             /* These are the operations that are available from this service. */
-            getTeamsList: function () { return getTeamsList() }
+            getTeamsList: function () { return getTeamsList() },
+            // post methods
+            saveNewTeam: function (newTeam) { return saveNewTeam(newTeam) }
         };
 
         return service;
 
+        //#region Post Methods
+        function saveNewTeam(newTeam) {
+            var wkUrl = config.apiUrl + 'saveTeam/';
+            return $http({
+                url: wkUrl,
+                params: { newTeam: newTeam },
+                method: 'POST',
+                isArray: true
+            }).then(success, fail)
+            function success(resp) {
+                return resp.data;
+            }
+            function fail(error) {
+                var msg = "Error getting  list: " + error;
+                //log.logError(msg, error, null, true);
+                throw error; // so caller can see it
+            }
+        }
+        //#endregion Post Methods
         //get  list on the basis on 
         function getTeamsList() {
             var wkUrl = config.apiUrl + 'teamsList/';
